7353fdef0571b10569aa18becab99e68fbea03c3
KangMyeungJun <zkzkfkcl23@naver.com>
2022-07-11 17:37:53 +0900

    commit

--- src/main/java/com/mwork/main/home/controller/MainController.java
+++ src/main/java/com/mwork/main/home/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.mwork.main.home.controller;
 
+import com.mwork.main.entity.member.Member;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
